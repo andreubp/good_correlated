c3541e872ac264d217b8e8dc0bbfa7c3149dff1a
Andreu Bofill Pumarola <chimp@ciutadella-136-217.eduroamcc.upf.edu>
2016-03-18 14:35:19 +0100

    all

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
 setup(name='correlate',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIaT',
-      scripts=['bin/main.py'],
+      version='1.0',
+      description='Correlated Mutation Tool',
+      url='https://github.com/andreubp/good_correlated.git',
+      author='Andreu Bofill & Marina Reixachs',
+      author_email='andreu.bofill@gmail.com',
+      license='CMP',
+      install_requires=[
+        'numpy',
+        'Biopython',
+        'matplotlib',
+        'plotly',
+      ],
+      scripts=['bin/correlate'],
       packages=['modules'],
       zip_safe=False)
